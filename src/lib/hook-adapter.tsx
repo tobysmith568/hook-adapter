7343ff1b2ab2b03e83389e7a1caa2ba6b0796675
Toby Smith <tobysmith568@hotmail.co.uk>
2025-02-15 17:12:04 +0000

    Add ESLint and bump packages

--- src/lib/hook-adapter.tsx
+++ src/lib/hook-adapter.tsx
@@ -1,4 +1,5 @@
-﻿import { ReactNode } from "react";
+﻿/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ReactNode } from "react";
 
 export type HookAdapterProps<THook extends (...args: any) => any> = {
   hook: THook;
